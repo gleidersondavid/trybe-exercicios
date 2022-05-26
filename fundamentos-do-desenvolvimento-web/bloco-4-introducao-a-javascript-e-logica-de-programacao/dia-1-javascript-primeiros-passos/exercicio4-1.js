dbe48d93ea852e64cecf07dcf7b4f667a16b9caa
Gleiderson David <gleidersondavid@gmail.com>
2022-05-25 23:41:05 -0300

    Adiciona exercicios de 1 ao 3

---
+++ fundamentos-do-desenvolvimento-web/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio4-1.js
@@ -0,0 +1,40 @@
+// exercicio1
+const a = 5;
+const b = 10;
+const c = 15;
+
+
+// var adicao = a + b;
+// console.log(adicao);
+
+// var subtracao = a - b;
+// console.log(subtracao);
+
+// var multiplicacao = a * b;
+// console.log(multiplicacao);
+
+// var divisao = a / b;
+// console.log(divisao);
+
+// var modulo = a % b;
+// console.log(modulo);
+
+
+
+// exercicio2
+// if (a > b) {
+//   console.log(a);
+// } else {
+//   console.log(b)
+// }
+
+
+// exercicio3
+// if (a > b && a > c) {
+//   console.log(a);
+// } else if (b > a && b > c) {
+//   console.log(b);
+// } else {
+//  console.log(c);
+// }
+
